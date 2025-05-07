f103a67c1e80457ff8a1de6ff245486e54b278a0
ramiroschettino <ramiroschettino1@hotmail.com>
2025-05-07 17:43:27 -0300

    Estructurando el proyecto y docker-compose

---
+++ src/main/java/com/example/user_management/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.user_management.repository;
+import com.example.user_management.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
